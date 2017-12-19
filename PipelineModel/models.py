40cd0bb8fbec999455394285daaf858d50fdf201
Wang <chenchun.wang@nokia-sbell.com>
2017-12-19 10:40:11 +0800

    test

--- PipelineModel/models.py
+++ PipelineModel/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class  Feature(models.Model):
+class Feature(models.Model):
 	name = models.CharField(max_length=200)
 	other = models.CharField(max_length=200)
 
-class  Parameter(models.Model):
+class Parameter(models.Model):
 	name = models.CharField(max_length=200)
 	display_name = models.CharField(max_length=200)
 	forms = models.CharField(max_length=200)
@@ -13,5 +13,16 @@ class  Parameter(models.Model):
 
 class Template(models.Model):
 	name = models.CharField(max_length=200)
-	parameter_list = 
-		
+
+class Pipeline(models.Model):
+	feature_id = models.ForeignKey(to='Feature')
+	template_ip = models.ForeignKey(to='Template')
+
+class TemplateParameter(models.Model):
+	template_id = models.ForeignKey(to='Template')
+	parameter_id = models.ForeignKey(to='Parameter')
+
+class PipelineParameter(object):
+	pipeline_id = models.ForeignKey(to='Pipeline')
+	template_parameter_id = models.ForeignKey(to='TemplateParameter')
+	value = models.TextField()
